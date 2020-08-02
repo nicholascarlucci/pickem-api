13021985b1b7d65b0ad9adbc7e50f23f739b37e9
nicholascarlucci <nicholas.r.carlucci@gmail.com>
2020-08-02 09:41:07 -0400

    initial commit for api

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weekly Pick'em API
+
+## Description:
+
+Node + Express back-end API that will allow us to connect front end to PostgreSQL database
+
+## Database setup:
+
+- Install PostgreSQL for Windows [here](https://www.postgresql.org/download/windows/)
+- TODO: script out initial database creation
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const team_model = require("./models/team_model");
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  team_model
+    .getTeams()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/teams", (req, res) => {
+  team_model
+    .createTeam(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/teams/:id", (req, res) => {
+  team_model
+    .deleteTeam(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
